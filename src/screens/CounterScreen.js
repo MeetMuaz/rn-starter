75cf4ac407f356b1ce9660b999d0c9345ae0fcaf
meetmuaz <muazbalogun97@gmail.com>
2023-12-20 13:15:37 +0100

    useReducer_counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,33 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 export default function CounterScreen() {
-    const [count, setCount] = useState(0);
 
-    const handleIncrementCount = () => {
-        setCount(count + 1);
-    }
+  const COLOR_INCREMENT = 1;
 
-    const handleDecrementCount = () => {
-        setCount(count - 1);
+  const reducer = (state, action) => {
+    // state = { count: number }
+    // action = { type: 'increament' || 'decreament', payload: 1 }
+
+    switch(action.type) {
+      case 'increment':
+        return { ...state, count: state.count + action.payload }
+      case 'decrement':
+        return state.count - action.payload < 0 ? state : { ...state, count: state.count - action.payload }
+      default:
+        return state;
     }
+  }
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const { count } = state;
+    
   return (
     <View>
       <Text>Count: {count}</Text>
-      <Button title='Increase' onPress={handleIncrementCount}/>
-      <Button title='Decrease' onPress={handleDecrementCount}/>
+      <Button title='Increase' onPress={() => dispatch({ type: 'increment', payload: COLOR_INCREMENT })}/>
+      <Button title='Decrease' onPress={() => dispatch({ type: 'decrement', payload: COLOR_INCREMENT })}/>
     </View>
   )
 }
